e7be2d64a7f04e7bd439b7fc583c5621e954e5f9
Bruno Gomes <bgomes.89@gmail.com>
2024-02-22 08:57:19 -0300

    Ajuste posicionamento div

--- src/components/PageDev/index.tsx
+++ src/components/PageDev/index.tsx
@@ -3,7 +3,7 @@ import img from "../../assets/manutencao.svg"
 
 export function Dev() {
     return (
-      <div className="flex flex-col items-center justify-center inset-0 gap-4 my-8">
+      <div className="flex flex-col items-center justify-center gap-4 my-8 fixed left-1/2  top-1/2 -translate-x-1/2 -translate-y-1/2 ">
         <div className="flex flex-col items-center justify-center gap-5">
           <img src={img} alt="imagem de um dev codando" className="w-[400px] "/>
           <h1 className="font-medium text-gray-600">Pagina em desenvolvimento!</h1>
