e0ee17d7ff9525ef4596cff4d4f4b9b5d86d7a65
Bruno Gomes <obrunogomes@obrunogomes.local>
2024-11-09 18:41:07 -0300

    ajuste sobre

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -3,10 +3,10 @@ export function Navbar(){
     return(
         <nav className="sm:flex hidden">
             <ul className="flex gap-6 text-base">
-                <li><a href="#home" className="hover:border-green hover:border-b-4 transition-all">Home</a></li>
-                <li><a href="#sobre" className="hover:border-green hover:border-b-4 transition-all">Sobre</a></li>
-                <li><a href="#projetos" className="hover:border-green hover:border-b-4 transition-all">Projetos</a></li>
-                <li><a href="#footer" className="hover:border-green hover:border-b-4 transition-all">Contato</a></li>
+                <li><a href="#home" className="hover:border-green hover:border-b-4 transition-all ">Home</a></li>
+                <li><a href="#sobre" className="hover:border-green hover:border-b-4 transition-all ">Sobre</a></li>
+                <li><a href="#projetos" className="hover:border-green hover:border-b-4 transition-all ">Projetos</a></li>
+                <li><a href="#footer" className="hover:border-green hover:border-b-4 transition-all ">Contato</a></li>
             </ul>
         </nav>
     )
--- src/page/Sobre/index.tsx
+++ src/page/Sobre/index.tsx
@@ -4,10 +4,22 @@ import { Title } from "../../components/ui/title";
 import { Destaque } from "../../components/ui/destaque";
 
 export function Sobre() {
+
+  function getAge(dateString: any) {
+    const today = new Date();
+    const birthDate = new Date(dateString);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+    
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return Number(age);
+}
     return (
         
         <section id="sobre" className="w-full h-[1280px] sm:h-svh py-28 flex items-center justify-center">
-          <div className="w-full sm:max-w-[1080px] h-[600px] m-auto p-10 flex flex-col gap-8">
+          <div className="w-full sm:max-w-[1080px] h-[1000px] m-auto p-10 flex flex-col gap-8">
             <Title title="Sobre" />
             <div className=" flex flex-col sm:flex-row gap-8 p-5 text-black">
               <div className="w-full sm:w-1/2 flex items-center justify-center">
@@ -15,16 +27,18 @@ export function Sobre() {
               </div>
               <div className="w-full sm:w-1/2 flex flex-col justify-center font-JMH gap-4 text-gray-600">
                 <p>
-                  Me chamo <Destaque text="Bruno" />, Cristão, tenho 35 anos, sou casado e pai da <Destaque text="Manu"/>!
+                  Me chamo <Destaque text="Bruno" />, Cristão, tenho {getAge('1989/01/16')} anos, sou casado e pai da <Destaque text="Manu"/>!
                 </p>
                 <p>
                   Sou estudante de <Destaque text="Sistemas para Internet" />, e focado em <Destaque text="Desenvolvimento Web frontend" />.
                 </p>
                 <p>
-                  Durante minha jornada, já atuei como lider de equipe, gerenciando toda parte de processos de uma assistencia técnica em smartphones, atualmente
-                  estou focado nos estudos em desenvolvimento web com foco no front utilizando as tecnológias: <Destaque text="React, Tailwind, JS, TS" />.
+                  Ao longo da minha trajetória profissional, já exerci a função de líder de equipe, onde fui responsável pelo gerenciamento de processos em uma assistência técnica
+                   de smartphones. Essa experiência fortaleceu minhas habilidades em liderança, gestão de projetos e resolução de problemas. Atualmente, meu foco está no aprofundamento
+                    em desenvolvimento web, especialmente no front-end, utilizando tecnologias como: <Destaque text="React, Tailwind, JS, TS" />.
                 </p>
-                <p>Atuo tambêm com desenvolvimento em Wordpress com criação de paginas!</p>
+                <p>Além disso, atuo com desenvolvimento em <Destaque text="Wordpress" />, criando páginas que aliam desempenho e design para uma experiência de usuário otimizada.
+                   Estou em constante aprendizado e atualização para acompanhar as melhores práticas e inovações no campo da tecnologia e do desenvolvimento web.</p>
               </div>
             </div>
             <div className=" w-full h-20 ">
